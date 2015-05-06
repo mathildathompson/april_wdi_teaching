4ed83485bb24885f76b2cfc2eb4d4b665cab8bb9
mathildathompson <mathildathompson288@gmail.com>
2015-05-06 08:54:27 +0100

    Added my jQuery lesson

--- week_2/jQuery-class-practice 4/js/script.js
+++ week_2/jQuery-class-practice 4/js/script.js
@@ -1,33 +1,48 @@
-var myApp = myApp || {}
+// var myApp = myApp || {}
 
-myApp.showSecretMessage = function(){
-	$('.secret_message').slideToggle(1000);
-}
+// myApp.showSecretMessage = function(){
+// 	$('.secret_message').slideToggle(1000);
+// }
 
-myApp.setUpEventListeners = function(){
-	$('li').on('mouseover', function(){
-		$('li').removeClass('active');
-		$(this).addClass('active');
+// myApp.setUpEventListeners = function(){
+// 	$('li').on('mouseover', function(){
+// 		$('li').removeClass('active');
+// 		$(this).addClass('active');
+// 	});
+
+// 	$('.picture').on('mouseover', function(){
+// 		$(this).attr('src', 'images/paulirish.jpg')
+// 	}).on('mouseout', function(){
+// 		$(this).attr('src', 'http://www.legaltechnology.com/wp-content/uploads/2013/06/timbernerslee.jpg')
+// 	});
+
+// 	$('#helloButton').on('click', myApp.showSecretMessage)
+// }
+
+// myApp.initialise = function(){
+// 	$('section').removeClass('no_js');
+// 	$('p.message').addClass('hidden');
+// }
+
+// $(document).ready(function(){
+// 	myApp.initialise()
+// 	myApp.setUpEventListeners();
+// })
+
+var setUpEventListeners = function(){
+	var helloButton = document.getElementById('helloButton');
+	helloButton.addEventListener('click', function(){
+		alert('helloWorld');
 	});
+};
 
-	$('.picture').on('mouseover', function(){
-		$(this).attr('src', 'images/paulirish.jpg')
-	}).on('mouseout', function(){
-		$(this).attr('src', 'http://www.legaltechnology.com/wp-content/uploads/2013/06/timbernerslee.jpg')
-	});
+var initialize = function(){
+	document.getElementsByTagName('section')[0].className = "";
+	document.getElementsByClassName('message')[0].className = "hidden";
+};
 
-	$('#helloButton').on('click', myApp.showSecretMessage)
-}
-
-myApp.initialise = function(){
-	$('section').removeClass('no_js');
-	$('p.message').addClass('hidden');
-}
-
-$(document).ready(function(){
-	myApp.initialise()
-	myApp.setUpEventListeners();
-})
+initialize();
+setUpEventListeners();
 
 
 
